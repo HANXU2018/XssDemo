92e15ab646e4c5aa524b2cfee0965619b6d1f233
HANXU2018 <1076998404@qq.com>
2021-05-09 16:44:17 +0800

    项目初始化

---
+++ XssWebDemo/src/main/java/com/example/demo/Controller/testController.java
@@ -0,0 +1,18 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@ResponseBody
+public class testController {
+
+    @RequestMapping(value = "/greeting")
+    public ModelAndView test(ModelAndView mv) {
+        mv.setViewName("/greeting");
+        mv.addObject("title","欢迎使用Thymeleaf!");
+        return mv;
+    }
+}
